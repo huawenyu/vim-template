a50f17af1a9b4e00c412164005c5d2e3e6e286d5
Huawen Yu <hyu@fortinet.com>
2025-02-10 22:25:49 -0800

    vim-plug: update vimConfig.tmux-navigate change to A-hjkl

--- templates/=template=.bash
+++ templates/=template=.bash
@@ -1,9 +1,12 @@
-#! /bin/bash
+#!/usr/bin/env bash
 # vim: setlocal autoindent cindent et ts=4 sw=4 sts=4:
 #
 # Get the script's directory, resolving symlinks
 var_ScriptDir="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )"
+var_ScriptName=$(basename "$0")
+var_ScriptName="${var_ScriptName%.*}"
 var_WorkDir=$(pwd)
+SECONDS=0
 
 _Usage=$(cat <<-END
     Usage: handle a files
@@ -17,69 +20,66 @@ _Usage=$(cat <<-END
       --version             Version
 
     Sample:
-      bash -x script        ### Run the script with debug mode enabled.
-      bash -n script        ### Check for syntax errors without execution.
+      bash -x script        # Run the script with debug mode enabled.
+      bash -n script        # Check for syntax errors without execution.
 
       script afile
 
 END
 )
 
-type getoptions 2>&1 > /dev/null || \
-    (echo "Require 'getoptions': <===???"; \
-    echo "Install getoptions (https://github.com/ko1nksm/getoptions)"; \
-    echo "wget https://github.com/ko1nksm/getoptions/releases/latest/download/getoptions -O $HOME/bin/getoptions"; \
-    echo ""; echo "$_Usage"; \
-    exit 2;)
-
-# Handle option {{{1}}}
-function parser_definition() {
-  setup   REST help:usage -- "Usage: git worktree2 [options] [branch-name] [dest-work-dir] ..." ''
+parse_define() {
+  setup   REST help:usage -- "Usage: $var_ScriptName [options] ..." ''
 
   msg -- 'Options:'
   flag  var_VERBOSE    -v --verbose  counter:true init:=0    -- "e.g. -vvv is verbose level 3"
   flag  var_DRYRUN     -n --dryrun                           -- "Dryrun mode"
   flag  var_DEBUG      -d --debug                            -- "Debug mode and list all local variable"
-  param var_ACTION     -a --action   init:="switch"  pattern:"switch | new | pull | push | delete"     -- "Action: *switch|new|pull|push|delete"
+  param var_Action     -a --action   init:="switch"  pattern:"switch | new | pull | push | delete"     -- "Action: *switch|new|pull|push|delete"
 
   disp  VERSION        --version
   disp  :usage         -h --help   -- "$_Usage"
 }
 
-eval "$(getoptions parser_definition) exit 1"
-var_RestArg="$@" # rest arguments
 
-
-DoIt () {
-    >&2 echo "    DO: $*"
-    eval "$@"
-}
-
-main () {
-    # Handle extra args out-of-getoptions
+# Parse manually if no getoptions
+parse_args () {
     case "$#" in
         2)
+            var_Action="$1"
+            var_SubAct="$2"
             ;;
         1)
-            if [ -z "$var_branchName" ]; then
-                var_branchName="$1"
-            fi
+            var_Action="$1"
             ;;
         0)
-            if [ -n "$GIT_DIR" ]; then
-                set -- "$GIT_DIR"
-            else
-                set -- .
-            fi
             ;;
         *)
-            if [[ -z "$var_DEBUG" ]]; then
-                die "Too many arguments"
-            fi
+            echo "$_Usage"
+            die "Incorrect arguments"
             ;;
     esac
 
+    # Help
+    while getopts "h" opt; do
+        case $opt in
+            h)
+                echo "$_Usage"
+                exit 0
+                ;;
+        esac
+    done
 
+}
+
+
+# Usage: varNum1=$( int var1 )
+int ()
+{
+    printf '%d' $(expr ${1:-} : '[^0-9]*\([0-9]*\)' 2>/dev/null) || :
+}
+
+do_args () {
     if [[ -n "$var_DRYRUN" ]]; then
         ( set -o posix ; set ) | grep -e '^var_'
         exit 1
@@ -92,6 +92,11 @@ main () {
     if [[ "$var_VERBOSE" -ge 4 ]]; then
         set -v                              ### Print each line of the script before eval
     fi
+}
+
+
+main () {
+    do_args
 
     ### Normalize the args
     # set --                                ### Clears positional parameters
@@ -127,21 +132,19 @@ die () {
     exit 1
 }
 
-TEMPFILE=
 
 maketemp () {
+    TEMPFILE=
     if [ -z "$TEMPFILE" ]; then
         TEMPFILE="$(mktemp /tmp/git-info.XXXXXX)" || die
-        trap "cleanup; exit 130" 1 2 3 15
     fi
 
     if [ -z "$TEMPFILE2" ]; then
         TEMPFILE2="$(mktemp /tmp/git-info.XXXXXX)" || die
-        trap "cleanup; exit 130" 1 2 3 15
     fi
 }
 
-trap cleanup EXIT INT TERM
+
 function cleanup() {
     trap - EXIT
 
@@ -159,6 +162,15 @@ function cleanup() {
     # Backto the original current dir
     cd "$var_WorkDir"
 }
+
+
+# wget -O $HOME/bin/getoptions https://github.com/ko1nksm/getoptions/releases/latest/download/getoptions
+trap "cleanup; exit 130" 1 2 3 15
+if command -v getoptions &>/dev/null; then
+    eval "$(getoptions parse_define) exit 1"
+else
+    parse_args "$@"
+fi
 
 main "$@"
 
